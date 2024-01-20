f2c6a3bc051353ebac575c76226c9fe051c0c484
Kbalbinott <157076775+Kbalbinott@users.noreply.github.com>
2024-01-19 22:04:58 -0300

    Add files via upload

---
+++ Palindromo/Palindromoexe.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindromo
+{
+    internal class DelegateFunAnonima2
+    {
+        delegate string StringOperacao(string s);
+
+        public static void Main1()
+        {
+            Console.WriteLine("Digite uma Palavra");
+            string palavra = Console.ReadLine();
+
+            StringOperacao inverter = delegate (string s)
+            {
+                char[] charArray = s.ToCharArray();
+                Array.Reverse(charArray);
+                return new string(charArray);
+            };
+
+            Console.WriteLine(inverter("RADAR"));
+
+            if (palavra == inverter(palavra))
+            {
+                Console.WriteLine($"Parabéns  {palavra} é um palindromo)");
+
+            }
+            else
+            {
+
+                Console.WriteLine($"{palavra},não é palindromo");
+            }
+        }
+    }
+}
---
+++ Palindromo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindromo
+{
+    internal class DelegateFunAnonima
+    {
+        delegate string StringOperacao(string s);
+
+        public static void Main()
+        {
+            bool resposta;
+            Console.WriteLine("Digite uma Palavra");
+            string palavra = Console.ReadLine();
+        
+            StringOperacao inverter = delegate (string s)
+            {
+                char[] charArray = s.ToCharArray();
+                Array.Reverse(charArray);
+                return new string(charArray);
+            };
+
+            Console.WriteLine("Você tem certeza que é essa palavra");
+            string certeza = Console.ReadLine();
+            if (certeza == "sim")
+            {
+
+                resposta = (palavra == inverter(palavra));
+
+                  
+                if (resposta)
+                {
+                    Console.WriteLine($"parabéns {palavra}  é um palindromo");
+
+                }
+                else
+                {
+                    Console.WriteLine($" {palavra}  não é um palindromo");
+                }
+            }
+            else if (certeza == "nao")
+            {
+                Console.WriteLine("Executando...");
+                Console.WriteLine($"Reinicie o progama.");
+            }
+        }
+    }
+}
